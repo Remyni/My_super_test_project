e316dfb1623ccf5805dae1e93d7e617f391a2597
Remyni <remyni.montalto@gmail.com>
2022-01-24 11:00:19 -0500

    Version finale

---
+++ lib/03_basics.rb
@@ -0,0 +1,27 @@
+def who_is_bigger ( a , b , c)
+
+    if a !=  nil && b !=  nil && c != nil 
+        if b > a ; c 
+            return "b is bigger"
+        elsif c > a ; b  
+            return "c is bigger"
+
+        else
+            return "a is bigger"
+        end
+    else "nil detected"
+    end
+end
+
+def reverse_upcase_noLTA
+    
+end
+
+def array_42(array)
+    if array.include? (42)
+        return true
+    else
+        return false
+    end
+end
+
